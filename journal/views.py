afd5af852f8e908819e41f2ec9ba18f0261ea0ae
Dmitry <dim27272929@gmail.com>
2019-01-23 20:20:13 +0400

    'filter fields'

--- journal/views.py
+++ journal/views.py
@@ -1,15 +1,16 @@
 # journal/views.py
+import django_filters
 from rest_framework import generics
 from .models import Journal
 from .serializers import JournalSerializer
-from django_filters import rest_framework as filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class JournalList(generics.ListCreateAPIView):
     queryset = Journal.objects.all()
     serializer_class = JournalSerializer
-    # filter_backends = (filters.DjangoFilterBackend,)
-    # filter_fields = ('cnt',)
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('product__is_new',)
 
 
 class JournalDetail(generics.RetrieveUpdateDestroyAPIView):
--- product/views.py
+++ product/views.py
@@ -12,7 +12,3 @@ class ProductList(generics.ListCreateAPIView):
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-
-
-
